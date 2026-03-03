cb904db7e17ed23d7806da8a5ec5c2275d002612
eimiii1 <philipjohn1627@gmail.com>
2026-03-03 11:34:06 +0800

    refactor: improve visual

--- Data Structures/Linked Lists/linked_lists.cpp
+++ Data Structures/Linked Lists/linked_lists.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 struct Node {
   std::string data;
@@ -9,8 +10,14 @@ void insertAtBeginning(Node *&head, std::string name);
 void printList(Node *head);
 
 int main() {
+  std::vector<std::string> names = {"Philip", "Jeanclaude Kalabaw", "John", "Junel", "Maria"};
   Node *head = nullptr;
-  insertAtBeginning(head, "Philip");
+  
+  for (std::string n : names) {
+    insertAtBeginning(head, n);
+  }
+  
+  printList(head);
 }
 
 void insertAtBeginning(Node *&head, std::string name) {
@@ -23,7 +30,7 @@ void insertAtBeginning(Node *&head, std::string name) {
 void printList(Node *head) {
   Node* temp = head;
   while (temp != nullptr) {
-    std::cout << temp->data;
+    std::cout << "\033[34m" << temp->data << "\033[0m";
     if (temp->next != nullptr) std::cout << " -> ";
     temp = temp->next;
   }
