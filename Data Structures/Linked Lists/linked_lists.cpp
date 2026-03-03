0335fcd9de93d593b1e464a88b2a3bde164b23b4
eimiii1 <philipjohn1627@gmail.com>
2026-03-03 11:20:35 +0800

    feat: Added a function for printing list

--- Data Structures/Linked Lists/linked_lists.cpp
+++ Data Structures/Linked Lists/linked_lists.cpp
@@ -1,22 +1,30 @@
 #include <iostream>
-using namespace std;
 
 struct Node {
-    string data;
-    Node* next;
+  std::string data;
+  Node *next;
 };
 
-void insertAtBeginning(Node*& head, string value);
-void print(Node* head);
+void insertAtBeginning(Node *&head, std::string name);
+void printList(Node *head);
 
 int main() {
-    Node* head = nullptr;
-    insertAtBeginning(head, "Philip");
+  Node *head = nullptr;
+  insertAtBeginning(head, "Philip");
 }
 
-void insertAtBeginning(Node*& head, string value) {
-    Node* newNode = new Node();
-    newNode->data = value;
-    newNode->next = head;
-    head = newNode;
+void insertAtBeginning(Node *&head, std::string name) {
+  Node *newNode = new Node();
+  newNode->data = name;
+  newNode->next = head;
+  head = newNode;
+}
+
+void printList(Node *head) {
+  Node* temp = head;
+  while (temp != nullptr) {
+    std::cout << temp->data;
+    if (temp->next != nullptr) std::cout << " -> ";
+    temp = temp->next;
+  }
 }
